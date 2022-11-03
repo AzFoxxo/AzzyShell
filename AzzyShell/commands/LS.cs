513627ca0a453a5993283f4f37719e20a57d9853
Az Foxxo <KnotGameStudio@gmail.com>
2022-11-03 23:44:59 +0000

    Imrpoved ls command

--- AzzyShell/commands/LS.cs
+++ AzzyShell/commands/LS.cs
@@ -7,23 +7,53 @@ public class LS : Command
     public override int Execute(string[] args)
     {
         // Check if args given
-        if (CheckArgLength(args, 1) != 0) return 2;
+        if (CheckArgLength(args, 1, allowGreaterThanLength: true) != 0) return 2;
 
         // Translate variables
         args = VariableTranslation(args);
 
-        // List all the directories in the current directory
-        Print("Directories:");
-        foreach (string dir in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+        // Current directory
+        var currentDirectory = Directory.GetCurrentDirectory();
+
+        // Get directory provided (if any)
+        if (args.Length == 2)
         {
-            Print(dir);
+            // Check if directory exists
+            if (!Directory.Exists(args[1]))
+            {
+                Print("Directory does not exist!", Colours.Red);
+                return 1;
+            }
+
+            // Set current directory to provided directory
+            currentDirectory = args[1];
         }
-        // List all the directories in the current directory
-        Print("Files:");
-        foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory()))
+        // Check for too many arguments
+        else if (args.Length > 2)
         {
-            Print(file);
+            Print($"Provide only argument (directory) not {args.Length-1}", Colours.Red);
+            return 2;
         }
+
+        // List all the directories in the current directory
+        foreach (var dir in Directory.GetDirectories(currentDirectory))
+        {
+            var directory = dir.Replace(currentDirectory, "").Remove(0, 1) + "/";
+            PrintPrompt($"{directory}, ", Colours.Blue);
+        }
+
+        // New line
+        Print("");
+
+        // List all the directories in the current directory
+        foreach (var file in Directory.GetFiles(currentDirectory))
+        {
+            var fileInfo = new FileInfo(file);
+            PrintPrompt($"{fileInfo.Name}, ", Colours.Green);
+        }
+
+        // New line
+        Print("");
 
 
         // Return success
