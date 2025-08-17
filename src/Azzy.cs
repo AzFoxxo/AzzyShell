338a25883a6f84d2ad55e1bf065325980d2abe2a
Az Foxxo <contact@azfoxxo.com>
2025-08-18 00:59:46 +0100

    Moved variable resolution out of command and into Azzy

--- src/Azzy.cs
+++ src/Azzy.cs
@@ -10,7 +10,7 @@ partial class Azzy : HeroesPatch
     private static Azzy? instance;
 
     // Constants
-    private const string version = "2.4.1";
+    private const string version = "2.4.2";
     private const string shell = "Azzy";
     private const string author = "Az Foxxo";
     private const string description = "A lightweight shell environment written in C#.";
@@ -275,7 +275,7 @@ partial class Azzy : HeroesPatch
         string[] resolvedCommand;
         try
         {
-            resolvedCommand = Command.VariableResolution(commandArguments);
+            resolvedCommand = VariableResolution(commandArguments);
         }
         catch (Exception ex)
         {
@@ -414,4 +414,77 @@ partial class Azzy : HeroesPatch
         return command;
     }
 
+     /// <summary>
+    /// Resolve variables
+    /// </summary>
+    /// <param name="args">Unresolved string array for command plus operands</param>
+    /// <returns>Resolved string array</returns>
+    /// <exception cref="ArgumentException">Thrown if an unrecognised opening bracket is encountered, or if a variable is not found, or if a closing bracket is missing.</exception>
+    public static string[] VariableResolution(string[] args)
+    {
+        if (Azzy.GetInstance() is null) Console.WriteLine("Azzy is null here!");
+        var variables = Azzy.GetInstance().Variables;
+
+        // Define opening to closing character map and what they extract
+        var modeMap = new Dictionary<char, Func<ShellVariable, string>>
+    {
+        { '{', v => v.Value },
+        { '[', v => v.Type },
+        { '(', v => v.Name },
+        { '<', v => variables.Keys.ToList().IndexOf(v.Name).ToString() }
+    };
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            var arg = args[i];
+
+            foreach (var open in modeMap.Keys)
+            {
+                var close = GetClosingChar(open);
+                int startIndex = 0;
+
+                while (startIndex < arg.Length)
+                {
+                    int openIndex = arg.IndexOf(open, startIndex);
+                    if (openIndex == -1) break;
+
+                    int closeIndex = arg.IndexOf(close, openIndex + 1);
+                    if (closeIndex == -1)
+                        throw new ArgumentException($"Unclosed variable placeholder: expected '{close}' after '{open}' in \"{arg}\".");
+
+                    // Span-based efficient slicing
+                    var span = arg.AsSpan();
+                    var varNameSpan = span.Slice(openIndex + 1, closeIndex - openIndex - 1);
+                    var varName = varNameSpan.ToString();
+
+                    if (!variables.TryGetValue(varName, out var variable))
+                        throw new ArgumentException($"Variable '{varName}' not found.");
+
+                    string replacement = modeMap[open](variable);
+
+                    // Efficient string replacement using spans
+                    arg = string.Concat(
+                        span[..openIndex],
+                        replacement,
+                        span[(closeIndex + 1)..]
+                    );
+
+                    startIndex = openIndex + replacement.Length;
+                }
+            }
+
+            args[i] = arg;
+        }
+
+        return args;
+
+        static char GetClosingChar(char open) => open switch
+        {
+            '{' => '}',
+            '[' => ']',
+            '(' => ')',
+            '<' => '>',
+            _ => throw new ArgumentException($"Unknown opening character: {open}")
+        };
+    }
 }
